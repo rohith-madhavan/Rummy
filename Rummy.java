806c641e28d90caa600d59e750583b380044f371
M <gajm@paypalcorp.com>
2016-08-10 18:18:34 +0530

    Added Rummy.java

---
+++ Rummy.java
@@ -0,0 +1,18 @@
+
+public class Rummy {
+	public Rummy(){
+		
+	}
+	public Boolean isSet(Hand h){
+		return false;
+	}
+	public Boolean isSequence(Hand h){
+		return false;
+	}
+	public Boolean canDeclare(Hand h){
+		return false;
+	}
+	public int cardsToDeclare(Hand h){
+		return 0;
+	}
+}
